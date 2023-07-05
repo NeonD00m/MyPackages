e7a333daca6c57c20789ca987827e779d12fe27c
NeonD00m <neonfamxxi@gmail.com>
2023-07-05 14:25:30 -0700

    Throttle v0.3.0: custom identifiers

--- Packages/Bunch.lua
+++ Packages/Bunch.lua
@@ -1,1 +1,1 @@
-return require(script.Parent._Index["neond00m_bunch@0.1.0"]["bunch"])
+return require(script.Parent._Index["neond00m_bunch@0.2.0"]["bunch"])
--- Packages/Throttle.lua
+++ Packages/Throttle.lua
@@ -1,1 +1,1 @@
-return require(script.Parent._Index["neond00m_throttle@0.1.0"]["throttle"])
+return require(script.Parent._Index["neond00m_throttle@0.3.0"]["throttle"])
